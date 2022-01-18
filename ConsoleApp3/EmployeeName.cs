137e96a482c8b107d74bfb712da31ea0285b0c2b
home <home@WINDOWS-PQ21EPG>
2022-01-18 20:50:39 +0530

    Add project files.

---
+++ ConsoleApp3/EmployeeName.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp3
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            EmployeeName myObj = new EmployeeName();
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class EmployeeName
+    {
+        string Name = "neela";
+        static void Main(string[] args)
+        {
+            EmployeeName myObj = new EmployeeName();
+            Console.WriteLine(myObj.Name);
+        }
+    }
+}
